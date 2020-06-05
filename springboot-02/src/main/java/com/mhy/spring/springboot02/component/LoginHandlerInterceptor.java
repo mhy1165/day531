4617dbf8d21bc030782e9eb536b63fb67daf4f15
mhy1165 <1165153082@qq.com>
2020-06-05 16:10:35 +0800

    默认更改列表

--- springboot-02/src/main/java/com/mhy/spring/springboot02/component/LoginHandlerInterceptor.java
+++ springboot-02/src/main/java/com/mhy/spring/springboot02/component/LoginHandlerInterceptor.java
@@ -12,18 +12,18 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginHandlerInterceptor implements HandlerInterceptor    /*[ˌɪntəˈseptə(r)]*/{
 
     //预检查，目标方法执行之前
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        Object user = request.getAttribute("loginUser");//属性是object
-        if(user == null){
-            //未登录返回登陆页面
-            request.setAttribute("msg","没有权限请先登录");
-            request.getRequestDispatcher("index.html").forward(request,response);
-            return false;
-        }else{
-            return true;
-        }
-    }
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        Object user = request.getAttribute("loginUser");//属性是object
+//        if(user == null){
+//            //未登录返回登陆页面
+//            request.setAttribute("msg","没有权限请先登录");
+//            request.getRequestDispatcher("index.html").forward(request,response);
+//            return false;
+//        }else{
+//            return true;
+//        }
+//    }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
